b59857c3bfeb2b7b1e2d8d9b346b656834964168
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 21:00:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :rent_in_dollars => {
-	      :one_piece_owned => ,
+	      :one_piece_owned => 25,
 	      key2: {},
 	      key3: {},
 	      key4: {}
