c4417ab68a5707fca02e60455cada92d5fc0c6de
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 21:05:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,7 @@ def monopoly_with_third_tier
 	    },
 	    :names => {
 	      :reading_railroad => {},
-	      :pennsylvania_railroa => {},
+	      :pennsylvania_railroad => {},
 	      key3: {},
 	      key4: {}
 	    
